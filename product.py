be00d2f4ba5d1d796279bc60d26a9d7de9e64b7a
Amol Ratnaparkhe <amolratnaparkhe@gmail.com>
2020-06-05 13:42:32 -0400

    product.py added

---
+++ product.py
@@ -0,0 +1,32 @@
+# Using division
+
+def prod_list(lst):
+    out = []
+    prod = 1
+    for i in range(len(lst)):
+        prod *= lst[i]
+    for i in range(len(lst)):
+        out.append(prod//lst[i])
+    return out
+
+#print(prod_list([1, 2, 3, 4, 5]))
+
+# Implement the function without using division
+
+def prod_wo_division(lst):
+    output = []
+    def prod(lst1):
+        product = 1
+        for i in range(len(lst1)):
+            product *= lst1[i]
+        return product
+    for i in range(len(lst)):
+        left = lst[:i]
+        right = lst[i+1:]
+        prod1 = prod(left)
+        prod2 = prod(right)
+        ans = prod1 * prod2
+        output.append(ans)
+    return output
+
+print(prod_wo_division([1, 2, 3, 4, 5]))
